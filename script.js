dd8098ed5b21ccc3914fd74b1cc419b2047b4e69
関達裕 <Tatsu@Tatsus-MacBook-Pro.local>
2016-03-05 18:12:07 +0900

    Q2-1

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(function(){
-
+	$('.shinki').css('border-radius','5px');
+	$('.shinki').click(function(){
+		var $answer = $('.answer')
+		if ($answer.hasClass('open')){
+			$answer.removeClass('open');
+			$answer.slideUp();
+			$(this).text('もっと詳しく');
+		} else {
+			$answer.addClass('open');
+			$answer.slideDown();
+			$(this).find('.shinki').slideDown();
+			$(this).text('閉じる');
+		};
+	});
 });
