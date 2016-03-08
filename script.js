6057ce2c1ab3287f7c58e75487bc83e56e29336c
関達裕 <Tatsu@Tatsus-MacBook-Pro.local>
2016-03-08 23:18:00 +0900

    Q3-1

--- script.js
+++ script.js
@@ -29,6 +29,13 @@ $(function(){
     return false;
     });
   });
+    $('.languages-box').hover(
+    	function(){
+    	$(this).find('.text-contents').addClass('text-active');
+    },
+    	function(){
+    	$(this).find('.text-contents').removeClass('text-active');
+    });
 });
 
 
