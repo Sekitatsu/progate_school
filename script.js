d919df9191d11b3072e67c774407d3465242bbbe
関達裕 <Tatsu@Tatsus-MacBook-Pro.local>
2016-03-09 20:20:14 +0900

    Q6-1

--- script.js
+++ script.js
@@ -12,30 +12,42 @@ $(function(){
 			$(this).text('閉じる');
 		};
 	});
+	
     $(function(){
    　// header aをクリックした場合に処理
     $('header a').click(function() {
     // スクロールの速度
      var speed = 400; // ミリ秒
-    // header aをクリックすると、href適用
-     var href= $(this).attr("href");
-     // 移動先を取得(offsetはdocument（左上）から、jQueryオブジェクトで指定した要素の相対位置（上）を取得)
-    // これはhrefのオブジェクトが頭にくることを示している？
+    // header aをクリックすると、href要素を取得する（例えば、#languagesなど）
+     var href= $(this).attr('href');
+    // #languagesが上からどのくらいの距離にあるかを数値で表示
     var position = $(href).offset().top;
-    // スムーススクロール $('要素名').animate({動かすプロパティ:目的地店の数値},{duration:速度,easing:アニメーションの動き})
-    // ブラウザの上端を（href付き）header aに合わせる（scrollTopの位置をpositionへ）
+    //$('要素名').animate({動かすプロパティ:目的地店の数値},{duration:速度,easing:アニメーションの動き})
+    // ブラウザの上端をを＃languagesの上からの距離に移動する
     $('body,html').animate({scrollTop:position}, speed);
     // clickイベントを実行した後にリンク先に移動してしまうことを防ぐ
     return false;
     });
   });
+  
     $('.languages-box').hover(
     	function(){
     	$(this).find('.text-contents').addClass('text-active');
     },
     	function(){
     	$(this).find('.text-contents').removeClass('text-active');
-    });
+  });
+  
+    
+    $('.column-box img').hover(function(){
+      $(this).css('zoom','1.5');
+    },function(){
+
+  });
+  
+  
+  
+  
 });
 
 
